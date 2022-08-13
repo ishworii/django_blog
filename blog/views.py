02442bdb28b16b468fbdbc685ed95c0268f53039
ishworii <ishworkhanal21@gmail.com>
2022-08-14 01:14:00 +0545

    Added database for Blog

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        "author": "CoreyMS",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "August 27, 2018",
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "August 28, 2018",
-    },
-]
+from .models import Post
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
