d3f8231b75ff13232d2d30c235fbd5f9a22cfa88
ishworii <ishworkhanal21@gmail.com>
2022-08-15 22:54:16 +0545

    Added login and logout routes

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -116,6 +116,9 @@ STATIC_URL = 'static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from .forms import UserRegisterForm
@@ -10,8 +11,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'You account has been created, now you can login.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
