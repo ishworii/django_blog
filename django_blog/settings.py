24713c47199191eb45957c085d8bf4a1e6af882d
ishworii <ishworkhanal21@gmail.com>
2022-08-18 19:58:42 +0545

    Added logic for email and password reset

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -125,3 +125,10 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'xxxxxxxxxxxxx'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxxx'
